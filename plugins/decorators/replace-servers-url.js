1534c6f23cc75edc532bc4ee13e8fc0682e419c7
Adam Botsford <abotsford@dropbox.com>
2022-07-07 10:48:35 -0500

    Debug

--- plugins/decorators/replace-servers-url.js
+++ plugins/decorators/replace-servers-url.js
@@ -8,7 +8,8 @@ function ReplaceServersURL() {
             leave(Server) {
                 console.log('present')
                 if ( 'SERVERS_URL' in process.env) {
-                    Server.url = process.env.SERVERS_URL;
+                    // Server.url = process.env.SERVERS_URL;
+                    console.log(Server);
                     console.log(Server.url);
                 }
 
