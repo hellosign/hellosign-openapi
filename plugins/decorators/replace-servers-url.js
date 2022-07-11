53174bdc31df83f94aa7e09f0494c63076396c80
Adam Botsfоrd <96210189+apb-dbx@users.noreply.github.com>
2022-07-11 16:47:08 -0500

    Conditionally set server in API reference doc based on environment (#97)
    Using custom plugin to conditionally set server

---
+++ plugins/decorators/replace-servers-url.js
@@ -0,0 +1,16 @@
+module.exports = ReplaceServersURL;
+
+/** @type {import('@redocly/cli').OasDecorator} */
+
+function ReplaceServersURL() {
+    return {
+        Server: {
+            leave(Server) {
+                if ( 'SERVERS_URL' in process.env) {
+                    Server.url = process.env.SERVERS_URL;
+                }
+
+            }
+        }
+    }
+}
