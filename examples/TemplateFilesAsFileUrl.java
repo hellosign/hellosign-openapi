30a76156497ad50d667b4dabc9bb5d3aa98beb20
fenghao <fenghao@dropbox.com>
2022-09-26 17:00:22 +0100

    Add examples to pass validation, will fix examples later

---
+++ examples/TemplateFilesAsFileUrl.java
@@ -0,0 +1,40 @@
+import com.hellosign.openapi.ApiClient;
+import com.hellosign.openapi.ApiException;
+import com.hellosign.openapi.Configuration;
+import com.hellosign.openapi.api.*;
+import com.hellosign.openapi.auth.HttpBasicAuth;
+import com.hellosign.openapi.model.*;
+
+public class Example {
+    public static void main(String[] args) {
+        ApiClient defaultClient = Configuration.getDefaultApiClient();
+
+        // Configure HTTP basic authorization: api_key
+        HttpBasicAuth api_key = (HttpBasicAuth) defaultClient
+            .getAuthentication("api_key");
+        api_key.setUsername("YOUR_API_KEY");
+
+        // or, configure Bearer (JWT) authorization: oauth2
+		/*
+		HttpBearerAuth oauth2 = (HttpBearerAuth) defaultClient
+            .getAuthentication("oauth2");
+
+        oauth2.setBearerToken("YOUR_ACCESS_TOKEN");
+		*/
+
+        TemplateApi api = new TemplateApi(defaultClient);
+
+        String templateId = "f57db65d3f933b5316d398057a36176831451a35";
+
+        try {
+            FileResponse result = api.templateFiles(templateId, "pdf", false, false);
+            System.out.println(result);
+        } catch (ApiException e) {
+            System.err.println("Exception when calling AccountApi#accountCreate");
+            System.err.println("Status code: " + e.getCode());
+            System.err.println("Reason: " + e.getResponseBody());
+            System.err.println("Response headers: " + e.getResponseHeaders());
+            e.printStackTrace();
+        }
+    }
+}
