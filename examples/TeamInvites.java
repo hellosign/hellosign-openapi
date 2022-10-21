d57ddd836b4906ce399b25a1cf62fd16d6bcfebc
Adam Botsford <abotsford@dropbox.com>
2022-10-21 13:27:43 -0500

    Fork Sync

---
+++ examples/TeamInvites.java
@@ -0,0 +1,40 @@
+import com.hellosign.openapi.ApiClient;
+import com.hellosign.openapi.ApiException;
+import com.hellosign.openapi.Configuration;
+import com.hellosign.openapi.api.*;
+import com.hellosign.openapi.auth.HttpBasicAuth;
+import com.hellosign.openapi.model.*;
+
+public class Example {
+    public static void main(String[] args) {
+        ApiClient defaultClient = Configuration.getDefaultApiClient();
+
+        // Configure HTTP basic authorization: api_key
+        HttpBasicAuth api_key = (HttpBasicAuth) defaultClient
+            .getAuthentication("api_key");
+        api_key.setUsername("YOUR_API_KEY");
+
+        // or, configure Bearer (JWT) authorization: oauth2
+		/*
+		HttpBearerAuth oauth2 = (HttpBearerAuth) defaultClient
+            .getAuthentication("oauth2");
+
+        oauth2.setBearerToken("YOUR_ACCESS_TOKEN");
+		*/
+
+        TeamApi api = new TeamApi(defaultClient);
+
+        String emailAddress = "user@hellosign.com";
+
+        try {
+            TeamInvitesResponse result = api.teamInvites(emailAddress);
+            System.out.println(result);
+        } catch (ApiException e) {
+            System.err.println("Exception when calling TeamApi#teamMembers");
+            System.err.println("Status code: " + e.getCode());
+            System.err.println("Reason: " + e.getResponseBody());
+            System.err.println("Response headers: " + e.getResponseHeaders());
+            e.printStackTrace();
+        }
+    }
+}
