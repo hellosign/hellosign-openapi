d128e3216279e5140ab923774ec4347d04be17c8
Juan Treminio <50673996+jtreminio-dropbox@users.noreply.github.com>
2022-11-16 15:12:08 -0600

    Updating SDK versions (#143)
    * 6.0.0-beta22.22 -> 6.0.0-beta22.23

--- sdks/java/src/main/java/com/hellosign/openapi/ApiClient.java
+++ sdks/java/src/main/java/com/hellosign/openapi/ApiClient.java
@@ -136,7 +136,7 @@ public class ApiClient extends JavaTimeFormatter {
     this.dateFormat = new RFC3339DateFormat();
 
     // Set default User-Agent.
-    setUserAgent("OpenAPI-Generator/6.0.0-beta22.22/java");
+    setUserAgent("OpenAPI-Generator/6.0.0-beta22.23/java");
 
     // Setup authentications (key: authentication name, value: authentication).
     authentications = new HashMap<String, Authentication>();
