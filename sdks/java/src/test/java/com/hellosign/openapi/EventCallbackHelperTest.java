5befea487f6f27afda3490acbb46e6e6f96cbe9c
Juan Treminio <juan.treminio@dropbox.com>
2022-11-28 17:56:58 -0600

    Refactor java

--- sdks/java/src/test/java/com/hellosign/openapi/EventCallbackHelperTest.java
+++ sdks/java/src/test/java/com/hellosign/openapi/EventCallbackHelperTest.java
@@ -6,7 +6,8 @@ import com.hellosign.openapi.model.EventCallbackApiAppRequestPayload;
 import org.junit.Assert;
 import org.junit.Test;
 
-import java.io.FileInputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class EventCallbackHelperTest {
     public static final String APIKEY = "324e3b0840f065eb51f3fd63231d0d33daa35d4ed10d27718839e81737065782";
@@ -14,11 +15,21 @@ public class EventCallbackHelperTest {
     @Test
     public void testIsValid() throws Exception {
         ObjectMapper mapper = JSON.getDefault().getMapper();
-        JsonNode content = mapper.readTree(new FileInputStream("test_fixtures/EventCallbackHelper.json"));
-        for(JsonNode node : content) {
-            EventCallbackApiAppRequestPayload payload = mapper.convertValue(node, EventCallbackApiAppRequestPayload.class);
+        JsonNode content = mapper.readTree(
+            Files.newInputStream(Paths.get("test_fixtures/EventCallbackHelper.json"))
+        );
+
+        for (JsonNode node : content) {
+            EventCallbackApiAppRequestPayload payload = mapper.convertValue(
+                node,
+                EventCallbackApiAppRequestPayload.class
+            );
+
             Assert.assertTrue(EventCallbackHelper.isValid(APIKEY, payload.getEvent()));
-            Assert.assertFalse(EventCallbackHelper.isValid(new StringBuilder(APIKEY).reverse().toString(), payload.getEvent()));
+            Assert.assertFalse(EventCallbackHelper.isValid(
+                new StringBuilder(APIKEY).reverse().toString(),
+                payload.getEvent())
+            );
         }
     }
 }
--- sdks/java/src/test/java/com/hellosign/openapi/SubFormFieldsPerDocumentTest.java
+++ sdks/java/src/test/java/com/hellosign/openapi/SubFormFieldsPerDocumentTest.java
@@ -6,7 +6,8 @@ import com.hellosign.openapi.model.SubFormFieldsPerDocumentBase;
 import org.junit.Assert;
 import org.junit.Test;
 
-import java.io.FileInputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.Iterator;
 import java.util.Map;
 
@@ -16,8 +17,11 @@ public class SubFormFieldsPerDocumentTest {
     @Test
     public void testSubFormFieldsPerDocumentBase() throws Exception {
         ObjectMapper mapper = JSON.getDefault().getMapper();
-        JsonNode content = mapper.readTree(new FileInputStream("test_fixtures//SubFormFieldsPerDocument.json"));
+        JsonNode content = mapper.readTree(
+            Files.newInputStream(Paths.get("test_fixtures//SubFormFieldsPerDocument.json"))
+        );
         Iterator<Map.Entry<String, JsonNode>> fields = content.fields();
+
         while (fields.hasNext()) {
             Map.Entry<String, JsonNode> kv = fields.next();
             String fieldName = kv.getKey();
@@ -28,7 +32,9 @@ public class SubFormFieldsPerDocumentTest {
 
             String serialized = mapper.writeValueAsString(base);
             JsonNode actual = mapper.readTree(serialized);
-            Assert.assertEquals(expected, actual);  // String comparison doesn't work due to json fields may be out of order
+
+            // String comparison doesn't work due to json fields may be out of order
+            Assert.assertEquals(expected, actual);
         }
     }
 }
