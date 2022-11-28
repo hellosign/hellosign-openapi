cfeed49dea06f730e91f7de27bb13108c71edd36
Juan Treminio <50673996+jtreminio-dropbox@users.noreply.github.com>
2022-11-28 12:10:43 -0600

    Sandbox setup for all SDKs (#156)
    * Sandbox setup for node, php, python, ruby
    
    * Sandbox setup for dotnet
    
    * Sandbox setup for java
    
    * Code review feedback

---
+++ sandbox/java/src/main/java/com/hellosign/sandbox/Main.java
@@ -0,0 +1,44 @@
+package com.hellosign.sandbox;
+
+import com.hellosign.openapi.ApiClient;
+import com.hellosign.openapi.ApiException;
+import com.hellosign.openapi.Configuration;
+import com.hellosign.openapi.api.*;
+import com.hellosign.openapi.auth.HttpBasicAuth;
+import com.hellosign.openapi.auth.HttpBearerAuth;
+import com.hellosign.openapi.model.*;
+
+public class Main {
+    public static void main(String[] args) {
+        ApiClient defaultClient = Configuration.getDefaultApiClient();
+
+        // Configure HTTP basic authorization: api_key
+        HttpBasicAuth api_key = (HttpBasicAuth) defaultClient
+            .getAuthentication("api_key");
+        api_key.setUsername("YOUR_API_KEY");
+
+        // or, configure Bearer (JWT) authorization: oauth2
+        /*
+        HttpBearerAuth oauth2 = (HttpBearerAuth) defaultClient
+            .getAuthentication("oauth2");
+
+        oauth2.setBearerToken("YOUR_ACCESS_TOKEN");
+        */
+
+        AccountApi api = new AccountApi(defaultClient);
+
+        AccountCreateRequest data = new AccountCreateRequest()
+            .emailAddress("newuser@hellosign.com");
+
+        try {
+            AccountCreateResponse result = api.accountCreate(data);
+            System.out.println(result);
+        } catch (ApiException e) {
+            System.err.println("Exception when calling AccountApi#accountCreate");
+            System.err.println("Status code: " + e.getCode());
+            System.err.println("Reason: " + e.getResponseBody());
+            System.err.println("Response headers: " + e.getResponseHeaders());
+            e.printStackTrace();
+        }
+    }
+}
