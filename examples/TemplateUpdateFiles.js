3e2c844e70b2982e50473af337b140d035387e20
Juan Treminio <50673996+jtreminio-dropbox@users.noreply.github.com>
2022-11-28 19:07:41 -0600

    Update Files endpoint examples; fix accessToken comment (#172)

--- examples/TemplateUpdateFiles.js
+++ examples/TemplateUpdateFiles.js
@@ -6,7 +6,7 @@ const api = new HelloSignSDK.TemplateApi();
 api.username = "YOUR_API_KEY";
 
 // or, configure Bearer (JWT) authorization: oauth2
-// $config->setAccessToken("YOUR_ACCESS_TOKEN");
+// api.accessToken = "YOUR_ACCESS_TOKEN";
 
 const data = {
   fileUrl: ["https://app.hellosign.com/docs/example_signature_request.pdf"],
