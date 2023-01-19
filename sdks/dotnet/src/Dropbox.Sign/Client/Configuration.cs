401d459fb90d018e793f451feb364884c0588c39
Juan Treminio <juan.treminio@dropbox.com>
2023-01-19 15:13:27 -0600

    Use PEP 440-friendly version number

--- sdks/dotnet/src/Dropbox.Sign/Client/Configuration.cs
+++ sdks/dotnet/src/Dropbox.Sign/Client/Configuration.cs
@@ -36,7 +36,7 @@ namespace Dropbox.Sign.Client
         /// Version of the package.
         /// </summary>
         /// <value>Version of the package.</value>
-        public const string Version = "1.0.0-beta23.1";
+        public const string Version = "1.0.0-beta2301";
 
         /// <summary>
         /// Identifier for ISO 8601 DateTime Format
@@ -118,7 +118,7 @@ namespace Dropbox.Sign.Client
         public Configuration()
         {
             Proxy = null;
-            UserAgent = WebUtility.UrlEncode("OpenAPI-Generator/1.0.0-beta23.1/csharp");
+            UserAgent = WebUtility.UrlEncode("OpenAPI-Generator/1.0.0-beta2301/csharp");
             BasePath = "https://api.hellosign.com/v3";
             DefaultHeaders = new ConcurrentDictionary<string, string>();
             ApiKey = new ConcurrentDictionary<string, string>();
@@ -550,7 +550,7 @@ namespace Dropbox.Sign.Client
             report += "    OS: " + System.Environment.OSVersion + "\n";
             report += "    .NET Framework Version: " + System.Environment.Version  + "\n";
             report += "    Version of the API: 3.0.0\n";
-            report += "    SDK Package Version: 1.0.0-beta23.1\n";
+            report += "    SDK Package Version: 1.0.0-beta2301\n";
 
             return report;
         }
--- sdks/ruby/lib/dropbox-sign/version.rb
+++ sdks/ruby/lib/dropbox-sign/version.rb
@@ -14,5 +14,5 @@ module Dropbox
 end
 
 module Dropbox::Sign
-  VERSION = '1.0.0-beta23.1'
+  VERSION = '1.0.0-beta2301'
 end
