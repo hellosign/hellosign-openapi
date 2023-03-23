2957cef7ef5b6fe5f46ee573068e9032137eecb0
Monica <monicaauriemma@icloud.com>
2023-03-23 17:05:16 -0400

    Updated from GET to POST

--- examples/EmbeddedEditUrl.sh
+++ examples/EmbeddedEditUrl.sh
@@ -1,2 +1,4 @@
-curl -X GET 'https://api.hellosign.com/v3/embedded/edit_url/{template_id}' \
-  -u 'YOUR_API_KEY:'
+curl -X POST 'https://api.hellosign.com/v3/embedded/edit_url/{template_id}' \
+  -u 'YOUR_API_KEY:' \
+  -F 'cc_roles[0]=' \
+  -F 'merge_fields=[]'
