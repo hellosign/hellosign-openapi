0bb60163390704337c99c877c999f3d3559d8a4e
Juan Treminio <juantreminio@dropbox.com>
2024-04-18 13:07:15 -0500

    Tag release 2024-04-18

--- sdks/node/types/api/apis.d.ts
+++ sdks/node/types/api/apis.d.ts
@@ -22,7 +22,7 @@ export interface returnTypeI {
 }
 export declare const queryParamsSerializer: (params: any) => any;
 export { RequestFile } from "../model";
-export declare const USER_AGENT = "OpenAPI-Generator/1.4.0/node";
+export declare const USER_AGENT = "OpenAPI-Generator/1.4.1/node";
 export declare const generateFormData: (obj: any, typemap: AttributeTypeMap) => {
     localVarUseFormData: boolean;
     data: object;
