2defbaa583c9d1a03dc04fa48ee6e716feadaedb
Juan Treminio <50673996+jtreminio-dropbox@users.noreply.github.com>
2024-04-18 13:09:08 -0500

    Tag release 2024-04-18 (#371)

--- sdks/node/api/apis.ts
+++ sdks/node/api/apis.ts
@@ -39,7 +39,7 @@ export const queryParamsSerializer = (params) => {
 
 export { RequestFile } from "../model";
 
-export const USER_AGENT = "OpenAPI-Generator/1.4.0/node";
+export const USER_AGENT = "OpenAPI-Generator/1.4.1/node";
 
 /**
  * Generates an object containing form data.
