66565ae91524abf777bf6dd1aede251291639a3c
Juan Treminio <juantreminio@dropbox.com>
2024-09-03 17:40:28 -0500

    Version bump

--- sdks/java-v1/src/main/java/com/dropbox/sign/Configuration.java
+++ sdks/java-v1/src/main/java/com/dropbox/sign/Configuration.java
@@ -15,7 +15,7 @@ package com.dropbox.sign;
 
 @javax.annotation.Generated(value = "org.openapitools.codegen.languages.JavaClientCodegen", comments = "Generator version: 7.8.0")
 public class Configuration {
-    public static final String VERSION = "1.5-dev";
+    public static final String VERSION = "1.6-dev";
 
     private static ApiClient defaultApiClient = new ApiClient();
 
