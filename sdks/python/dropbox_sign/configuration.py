e3afb19e54d5b5a73cbd1442f2c7b650096ced4d
Juan Treminio <50673996+jtreminio-dropbox@users.noreply.github.com>
2024-09-16 11:05:37 -0500

    Python: Shorting debug statement to prevent Black diff (#428)

--- sdks/python/dropbox_sign/configuration.py
+++ sdks/python/dropbox_sign/configuration.py
@@ -432,9 +432,7 @@ class Configuration:
             "OS: {env}\n"
             "Python Version: {pyversion}\n"
             "Version of the API: 3.0.0\n"
-            "SDK Package Version: 1.6-dev".format(
-                env=sys.platform, pyversion=sys.version
-            )
+            "SDK Version: 1.6-dev".format(env=sys.platform, pyversion=sys.version)
         )
 
     def get_host_settings(self):
