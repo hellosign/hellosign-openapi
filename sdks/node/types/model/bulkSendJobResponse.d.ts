14c3889efbfb952d4585182a7b979e65bfdcb5c5
sainishm2 <smomin+2@hellosign.com>
2024-10-11 15:08:59 -0700

    merge trunk

--- sdks/node/types/model/bulkSendJobResponse.d.ts
+++ sdks/node/types/model/bulkSendJobResponse.d.ts
@@ -1,9 +1,9 @@
 import { AttributeTypeMap } from "./";
 export declare class BulkSendJobResponse {
-    "bulkSendJobId"?: string | null;
-    "total"?: number;
-    "isCreator"?: boolean;
-    "createdAt"?: number;
+    "bulkSendJobId": string;
+    "total": number;
+    "isCreator": boolean;
+    "createdAt": number;
     static discriminator: string | undefined;
     static attributeTypeMap: AttributeTypeMap;
     static getAttributeTypeMap(): AttributeTypeMap;
