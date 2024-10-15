6a2a39b3a0a5165c39da60c99b8bd6cba7be8447
Juan Treminio <50673996+jtreminio-dropbox@users.noreply.github.com>
2024-10-15 13:17:32 -0500

    Improvements to Team response; reduces nullability (#439)

--- sdks/node/types/model/teamResponse.d.ts
+++ sdks/node/types/model/teamResponse.d.ts
@@ -1,10 +1,10 @@
 import { AttributeTypeMap } from "./";
 import { AccountResponse } from "./accountResponse";
 export declare class TeamResponse {
-    "name"?: string;
-    "accounts"?: Array<AccountResponse>;
-    "invitedAccounts"?: Array<AccountResponse>;
-    "invitedEmails"?: Array<string>;
+    "name": string;
+    "accounts": Array<AccountResponse>;
+    "invitedAccounts": Array<AccountResponse>;
+    "invitedEmails": Array<string>;
     static discriminator: string | undefined;
     static attributeTypeMap: AttributeTypeMap;
     static getAttributeTypeMap(): AttributeTypeMap;
