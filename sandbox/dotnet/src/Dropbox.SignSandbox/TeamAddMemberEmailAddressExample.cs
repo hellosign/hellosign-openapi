59a5335356794dfdfc6cf640395b706e5fdf74ef
Juan Treminio <juantreminio@dropbox.com>
2025-02-10 13:58:11 -0600

    Rename examples

---
+++ sandbox/dotnet/src/Dropbox.SignSandbox/TeamAddMemberEmailAddressExample.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+using Dropbox.Sign.Api;
+using Dropbox.Sign.Client;
+using Dropbox.Sign.Model;
+
+namespace Dropbox.SignSandbox;
+
+public class TeamAddMemberEmailAddressExample
+{
+    public static void Run()
+    {
+        var config = new Configuration();
+        config.Username = "YOUR_API_KEY";
+        // config.AccessToken = "YOUR_ACCESS_TOKEN";
+
+        var teamAddMemberRequest = new TeamAddMemberRequest(
+            emailAddress: "george@example.com"
+        );
+
+        try
+        {
+            var response = new TeamApi(config).TeamAddMember(
+                teamAddMemberRequest: teamAddMemberRequest,
+                teamId: "4fea99bfcf2b26bfccf6cea3e127fb8bb74d8d9c"
+            );
+
+            Console.WriteLine(response);
+        }
+        catch (ApiException e)
+        {
+            Console.WriteLine("Exception when calling Team#TeamAddMember: " + e.Message);
+            Console.WriteLine("Status Code: " + e.ErrorCode);
+            Console.WriteLine(e.StackTrace);
+        }
+    }
+}
