b9039d9a14dfe91faf31adf8688525b887ed3031
Juan Treminio <juantreminio@dropbox.com>
2025-02-10 14:00:37 -0600

    Rename java examples

---
+++ sandbox/java/src/main/java/com/dropbox/sign_sandbox/TeamInfoExample.java
@@ -0,0 +1,39 @@
+package com.dropbox.sign_sandbox;
+
+import com.dropbox.sign.ApiException;
+import com.dropbox.sign.Configuration;
+import com.dropbox.sign.api.*;
+import com.dropbox.sign.auth.*;
+import com.dropbox.sign.model.*;
+
+import java.io.File;
+import java.time.LocalDate;
+import java.time.OffsetDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class TeamInfoExample
+{
+    public static void main(String[] args)
+    {
+        var config = Configuration.getDefaultApiClient();
+        config.setUsername("YOUR_API_KEY");
+        // config.setAccessToken("YOUR_ACCESS_TOKEN");
+
+        try
+        {
+            var response = new TeamApi(config).teamInfo(
+                "4fea99bfcf2b26bfccf6cea3e127fb8bb74d8d9c"
+            );
+
+            System.out.println(response);
+        } catch (ApiException e) {
+            System.err.println("Exception when calling Team#teamInfo");
+            System.err.println("Status code: " + e.getCode());
+            System.err.println("Reason: " + e.getResponseBody());
+            System.err.println("Response headers: " + e.getResponseHeaders());
+            e.printStackTrace();
+        }
+    }
+}
