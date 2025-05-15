2dd505c041bfaca58f3582ef9db88a4989766e64
Juan Treminio <juantreminio@dropbox.com>
2025-05-15 10:30:21 -0500

    Python SDK - Download files from SignatureRequestApi is not working anymore; fixes #471

--- sdks/python/dropbox_sign/api_client.py
+++ sdks/python/dropbox_sign/api_client.py
@@ -302,6 +302,8 @@ class ApiClient:
         try:
             if response_type == "bytearray":
                 return_data = response_data.data
+            elif response_type == "io.IOBase":
+                return_data = self.__bytearray_to_iobase(response_data)
             elif response_type == "file":
                 return_data = self.__deserialize_file(response_data)
             elif response_type is not None:
@@ -690,6 +692,13 @@ class ApiClient:
             f.write(response.data)
 
         return path
+
+    def __bytearray_to_iobase(self, response):
+        """Convert bytearray to io.IOBase"""
+        buffer = io.BytesIO()
+        buffer.write(response.data)
+        buffer.seek(0)
+        return buffer
 
     def __deserialize_primitive(self, data, klass):
         """Deserializes string to primitive type.
