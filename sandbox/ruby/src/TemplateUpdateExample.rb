97ecab58889394161501aa36bc3c361b825c606f
Juan Treminio <juantreminio@dropbox.com>
2025-08-04 13:40:18 -0500

    Updates sandbox examples

--- sandbox/ruby/src/TemplateUpdateExample.rb
+++ sandbox/ruby/src/TemplateUpdateExample.rb
@@ -6,6 +6,19 @@ Dropbox::Sign.configure do |config|
     # config.access_token = "YOUR_ACCESS_TOKEN"
 end
 
+form_fields_1 = Dropbox::Sign::SubUpdateFormField.new
+form_fields_1.api_id = "uniqueIdHere_1"
+form_fields_1.name = "New name 1"
+
+form_fields_2 = Dropbox::Sign::SubUpdateFormField.new
+form_fields_2.api_id = "uniqueIdHere_2"
+form_fields_2.name = "New name 2"
+
+form_fields = [
+    form_fields_1,
+    form_fields_2,
+]
+
 template_update_request = Dropbox::Sign::TemplateUpdateRequest.new
 template_update_request.allow_form_view = false
 template_update_request.title = "Test Title"
@@ -15,6 +28,7 @@ template_update_request.cc_roles = [
     "CC Role 1",
     "CC Role 2",
 ]
+template_update_request.form_fields = form_fields
 
 begin
     response = Dropbox::Sign::TemplateApi.new.template_update(
